6b8a058dc4e19e39efeb49ee2a6cf5774a0c34fa
Alexander Altman <alexanderaltman@me.com>
2013-05-24 16:31:04 -0700

    Some more stuff
    [none]

--- src/Language/Elementscript/Micro.hs
+++ src/Language/Elementscript/Micro.hs
@@ -1,29 +1,46 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Language.Elementscript.Micro (tokenize,
-                                     recognize,
+                                     EvalState(..),
+                                     initialEvalState,
                                      evaluate,
-                                     Token(..),
                                      -- * Text/Pipes Utilities
                                      stdinST,
                                      stdoutDT) where
-import Data.Text.Lazy (Text)
-import qualified Data.Text.Lazy as T
+import Data.Text (Text)
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
 import Data.String (IsString(..))
-import Data.Map.Lazy (Map)
-import qualified Data.Map.Lazy as M
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Data.IntMap.Strict (Map)
+import qualified Data.Map.Strict as IntMap
+import Data.Sequence (Seq)
+import qualified Data.Sequence as Seq
+import Data.Set (Set)
+import qualified Data.Set as Set
 import Control.Proxy
+import Control.Proxy.Trans.State
 
-tokenize :: (Proxy p, Monad m) => () -> Pipe Text [[Text]] m r
-tokenize () = mapD
+tokenize :: Text -> Seq Text
+tokenize = S.fromList . T.words
+{-# INLINABLE tokenize #-}
+
+data EvalState p = ES { varMap :: Map Text (Seq Text -> () -> ), 
+                        precedenceList :: IntMap (Set Text) }
+
+initialEvalState :: EvalState
+initialEvalState = ES {  }
+
+evaluate :: (Proxy p) => () -> Consumer (StateP (EvalState p) p) (Seq Text) IO ()
 
 stdinST :: (Proxy p) => () -> Producer p Text IO r
-stdinST () = runIdentityP . forever $ do str <- lift T.getLine
+stdinST () = runIdentityP . forever $ do str <- lift Text.getLine
                                          respond str
-{-# INLINABLE stdinS #-}
+{-# INLINABLE stdinST #-}
 
 stdoutDT :: (Proxy p) => x -> p x String x String IO r
 stdoutDT = runIdentityK . foreverK $ \x -> do a <- request x
-                                              lift $ putStrLn a
+                                              lift $ Text.putStrLn a
                                               respond a
-{-# INLINABLE stdoutD #-}
+{-# INLINABLE stdoutDT #-}
 
--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,11 @@
 module Main (main) where
-import Data.Text.Lazy (Text)
-import qualified Data.Text.Lazy as T
-import qualified Data.Text.IO as T
-import Data.Map.Lazy (Map)
-import qualified Data.Map.Lazy as M
+import Data.Text (Text)
+import qualified Data.Text as Text
+import Data.Sequence (Seq)
+import qualified Data.Sequence as Seq
 import Control.Proxy
+import Control.Proxy.Trans.State
 import Language.Elementsong.Micro
 
 main :: IO ()
-main = runProxy $ stdinST >-> tokenize >-> recognize >-> evaluate 
+main = runProxy $ stdinST >-> tokenize >-> evalStateK initialEvalState evaluate
