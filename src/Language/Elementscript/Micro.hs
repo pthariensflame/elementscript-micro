bd30d3d69a8443b8421c7b647f22f9c58b5bf605
Alexander Altman <alexanderaltman@me.com>
2013-05-24 15:21:45 -0700

    Some module implementation
    [none]

--- src/Language/Elementscript/Micro.hs
+++ src/Language/Elementscript/Micro.hs
@@ -1,1 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Language.Elementscript.Micro (tokenize,
+                                     recognize,
+                                     evaluate,
+                                     Token(..),
+                                     -- * Text/Pipes Utilities
+                                     stdinST,
+                                     stdoutDT) where
+import Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy as T
+import Data.String (IsString(..))
+import Data.Map.Lazy (Map)
+import qualified Data.Map.Lazy as M
+import Control.Proxy
+
+tokenize :: (Proxy p, Monad m) => () -> Pipe Text [[Text]] m r
+tokenize () = mapD
+
+stdinST :: (Proxy p) => () -> Producer p Text IO r
+stdinST () = runIdentityP . forever $ do str <- lift T.getLine
+                                         respond str
+{-# INLINABLE stdinS #-}
+
+stdoutDT :: (Proxy p) => x -> p x String x String IO r
+stdoutDT = runIdentityK . foreverK $ \x -> do a <- request x
+                                              lift $ putStrLn a
+                                              respond a
+{-# INLINABLE stdoutD #-}
 
--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,11 @@
+module Main (main) where
+import Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.IO as T
+import Data.Map.Lazy (Map)
+import qualified Data.Map.Lazy as M
+import Control.Proxy
+import Language.Elementsong.Micro
 
+main :: IO ()
+main = runProxy $ stdinST >-> tokenize >-> recognize >-> evaluate 
