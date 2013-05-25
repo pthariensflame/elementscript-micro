3a8748d4fd1d9f00f742b8b36babc75861a381c9
Alexander Altman <alexanderaltman@me.com>
2013-05-24 18:33:55 -0700

    Data.Data and GHC.Generics
    [none]

--- src/Language/Elementscript/Micro/Values.hs
+++ src/Language/Elementscript/Micro/Values.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveDataTypeable, DeriveGeneric #-}
 module Language.Elementscript.Micro.Values (Val(..),
                                             PrimVal(..),
                                             OpTree(..)) where
@@ -8,15 +9,17 @@ import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import Text.Show
 import Text.Read
+import Data.Data
+import GHC.Generics
 
 data Val = Primitive PrimVal
          | Element (Map PrimVal Val)
          | Function (Val -> IO Val)
          | TreeMacro (OpTree -> IO Val)
-         | TextMacro (Text -> IO Val)
+         | TextMacro (Text -> IO Val) deriving (Typeable, Generic)
 
 data PrimVal = PrimInt Integer
-             | PrimText Text deriving (Eq, Ord)
+             | PrimText Text deriving (Eq, Ord, Typeable, Data, Generic)
 
 instance Show PrimVal where
   showsPrec d (PrimInt i) = showsPrec d i
@@ -29,12 +32,12 @@ instance Read PrimVal where
 instance IsString PrimVal where
   fromString = PrimText . Text.pack
 
-data OpTree = Variable { varName :: Text }
-            | PrimValue { getPrimValue :: PrimVal }
+data OpTree = Variable Text
+            | PrimValue PrimVal
             | FullApp { leftSubtree :: OpTree, 
                         opSubtree :: OpTree,
                         rightSubtree :: OpTree }
             | LeftSection { leftSubtree :: OpTree, 
                             opSubtree :: OpTree }
             | RightSection { opSubtree :: OpTree,
-                             rightSubtree :: OpTree } deriving (Eq, Read, Show)
+                             rightSubtree :: OpTree } deriving (Eq, Read, Show, Typeable, Data, Generic)
