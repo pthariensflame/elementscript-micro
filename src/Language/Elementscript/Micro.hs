a9ead610d8b5e7e018064d3a83fa2ff6da2766ea
Alexander Altman <alexanderaltman@me.com>
2014-04-29 04:22:26 +0000

    Start the complete revamping necessary

--- src/Language/Elementscript/Micro.hs
+++ src/Language/Elementscript/Micro.hs
@@ -1,10 +1,10 @@
+{-# LANGUAGE TypeHoles #-}
 module Language.Elementscript.Micro (initialEvalState,
                                      EvalState(..),
-                                     evaluate,
-                                     Val(..),) where
+                                     evaluate) where
 import Control.Monad.State
-import Data.Text (Text)
-import qualified Data.Text as Text
+import Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy as Text
 import Data.String (IsString(..))
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
@@ -13,32 +13,13 @@ import qualified Data.IntMap.Strict as IntMap
 import Data.Sequence (Seq)
 import qualified Data.Sequence as Seq
 
-data Val :: * where
-     Function :: (Val -> State EvalState Val) -> Val
-     Lambda   ::                                 Val
-     Declare  ::                                 Val
-     In       ::                                 Val
-     WithPrec ::                                 Val
+data AST :: * where
+    Value :: AST
 
-data EvalState = ES { valMap :: Map Text Val,
-                      precedenceMap :: IntMap Text }
+data EvalState = ES {}
 
 initialEvalState :: EvalState
-initialEvalState = ES { valMap = Map.fromList [ ("->"  , Lambda  )
-                                              , ("="   , Declare )
-                                              , ("in"  , In      )
-                                              , ("prec", WithPrec)
-                                              ]
-                      , precedenceMap = IntMap.fromList [ (0  , "->"  )
-                                                        , (100, "="   )
-                                                        , (300, "in"  )
-                                                        , (200, "prec")
-                                                        ]
-                      }
-
-lookupId :: Text -> State EvalState Val
-lookupId = undefined
+initialEvalState = _
 
 evaluate :: Seq Text -> State EvalState (Seq Text)
-evaluate input = do ES vM pM <- get
-                    undefined
+evaluate input = _
--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,11 @@
 module Main (main) where
-import Prelude hiding (interact)
-import Data.Text.Lazy.IO (interact)
+import Prelude hiding (interact, words, unwords)
+import Data.Text.Lazy
+import Data.Text.Lazy.IO
+import qualified Data.Sequence as Seq
 import Control.Monad.State
+import Data.Foldable (fold, toList)
 import Language.Elementscript.Micro
 
 main :: IO ()
-main = interact $ evalState evaluate 
+main = interact $ \input -> unwords . toList . flip evalState initialEvalState . evaluate . Seq.fromList $ words input
