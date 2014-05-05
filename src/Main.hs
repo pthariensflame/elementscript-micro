01f3033858e168d600a998d3a1d7ed60a734d9ff
Alexander Altman <alexanderaltman@me.com>
2014-05-05 21:32:24 +0000

    Some additional work

--- src/Main.hs
+++ src/Main.hs
@@ -2,12 +2,12 @@ module Main (main) where
 import           Data.Text.Lazy
 import           Data.Text.Lazy.IO
 import           Language.Elementscript.Micro
-import           Prelude                      hiding (getContents)
+import           Prelude                      hiding (getContents, putStrLn)
 import           System.IO                    (stderr)
 import           Text.Parsec
-import           Text.Parsec.Text.Lazy
+import           Text.Parsec.Text.Lazy        ()
 
 main :: IO ()
 main = getContents >>= runParserT evaluate initialEvalState "" >>= \case
     Left err -> hPutStrLn stderr . pack $ show err
-    Right val -> return . prettyPrint $ normalize val
+    Right val -> putStrLn $ prettyPrint $ normalize val
